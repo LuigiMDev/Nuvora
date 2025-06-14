43bc2c33805d0cadbddf4453d43b7430b3e1ece3
LuigiMDev <luigicumaru@gmail.com>
2025-06-14 13:27:20 -0300

    first commit: estrutura básica do backend e instalação do Prisma

---
+++ backend/src/PrismaService/prisma.service.ts
@@ -0,0 +1,24 @@
+import {
+  Injectable,
+  OnModuleInit,
+  OnModuleDestroy,
+  Global,
+} from '@nestjs/common';
+import { PrismaClient } from '@prisma/client';
+
+@Global()
+@Injectable()
+export class PrismaService
+  extends PrismaClient
+  implements OnModuleInit, OnModuleDestroy
+{
+  async onModuleInit() {
+    //eslint-disable-next-line @typescript-eslint/no-unsafe-call
+    await this.$connect();
+  }
+
+  async onModuleDestroy() {
+    //eslint-disable-next-line @typescript-eslint/no-unsafe-call
+    await this.$disconnect();
+  }
+}
