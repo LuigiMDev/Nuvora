841ddb394594e9a109a525427c9b89a7c07f92d6
LuigiMDev <luigicumaru@gmail.com>
2025-06-19 19:24:39 -0300

    feat:  buscar todas as orders

--- backend/src/order/order.controller.ts
+++ backend/src/order/order.controller.ts
@@ -1,13 +1,18 @@
-import { Body, Controller, Post, Req } from '@nestjs/common';
+import { Body, Controller, Get, Post, Req } from '@nestjs/common';
 import { OrderService } from './order.service';
 import { CreateOrderDTO } from 'src/DTOs/order';
 import { Request } from 'express';
 
-@Controller('order')
+@Controller('orders')
 export class OrderController {
   constructor(private readonly service: OrderService) {}
-  @Post('create')
+  @Post()
   async createOrder(@Body() orderData: CreateOrderDTO, @Req() req: Request) {
     return await this.service.createOrder(orderData, req);
+  }
+
+  @Get()
+  async getOrders(@Req() req: Request) {
+    return await this.service.getOrders(req)
   }
 }
