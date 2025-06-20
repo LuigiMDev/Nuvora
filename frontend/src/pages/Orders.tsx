6d0f904cc4049638da69377a4269e29476277bbf
LuigiMDev <luigicumaru@gmail.com>
2025-06-20 12:28:57 -0300

    style: retornar skeleton caso ainda esteja buscando o usuário nas orders

--- frontend/src/pages/Orders.tsx
+++ frontend/src/pages/Orders.tsx
@@ -72,11 +72,7 @@ export default function Orders() {
     }
   };
 
-  if (isCheckingAuth) {
-    return null;
-  }
-
-  if (isFindOrders) {
+  if (isFindOrders || isCheckingAuth) {
     return (
       <div className="max-w-4xl mx-auto px-4 sm:px-6 lg:px-8 py-8">
         <div className="mb-8">
